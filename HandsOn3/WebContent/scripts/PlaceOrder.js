e8a57431e123759b9b8b75d011f98579ce2c8c5d
dsin51 <dsinghal3@sapient.com>
2016-02-25 11:45:54 +0530

    First Ques

---
+++ HandsOn3/WebContent/scripts/PlaceOrder.js
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+
+function myFunction(){
+	
+	var ele = document.getElementById("name").value;
+	
+	prompt("Please Confirm your name:\n",ele);
+}
+
+function onSubmit(){
+	
+	var values = document.getElementById("name").value;
+	
+	var loc = document.getElementById("location");
+	var index = loc.selectedIndex;
+	var location = loc.options[index].text;
+	values = values + "\n" + location;
+	
+	var payMode;
+	if (document.getElementById("net").checked) {
+		payMode = document.getElementById("net").value;
+		} 
+	if (document.getElementById("visa").checked) {
+		payMode = document.getElementById("visa").value;
+		}
+	if (document.getElementById("card").checked) {
+		payMode = document.getElementById("card").value;
+		}
+	values = values + "\n" + payMode;
+	
+	
+	var checkedValue = "";
+	var inputElements = document.getElementsByClassName('messageCheckbox');
+	for(var i=0; inputElements[i]; i++){
+	      if(inputElements[i].checked){
+	           checkedValue = checkedValue + " " + inputElements[i].value;
+	      }
+	}
+	
+	values = values + "\n" + checkedValue;
+	
+	alert(values);
+}
